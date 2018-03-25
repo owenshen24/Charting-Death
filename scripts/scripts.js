1f1590a8e17e1b9c013941492853046e3f73dbc9
Owen Shen <owenshen24@gmail.com>
2018-03-24 18:50:58 -0700

    added

---
+++ scripts/scripts.js
@@ -0,0 +1,50 @@
+$( document ).ready(function() {
+
+  var google_path = 'data/new_google_normalized_trends.css';
+
+  $.get(google_path, function (data) {
+        var csvdata = Papa.parse(data);
+        console.log(csvdata);
+    });
+
+
+
+  var ctx = $("#myChart");
+  /*
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+  }); */
+});
